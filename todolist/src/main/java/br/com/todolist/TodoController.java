c2940faac5fa7d21b30f80e983411c837cdfe536
Daniel Santos <Daniel.gsantos@aluno.senai.br>
2023-08-24 17:04:33 -0300

    criando metodos para iniciar, finalizar, atualizar tarefa da api

--- todolist/src/main/java/br/com/todolist/TodoController.java
+++ todolist/src/main/java/br/com/todolist/TodoController.java
@@ -20,16 +20,18 @@ public class TodoController {
         this.todoRepo = todoRepo;
 
     }
+
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
-    public List<Todo> getAll(){
+    public List<Todo> getAll() {
         return this.todoRepo.findAll();
 
 
-        }
+    }
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public  Todo create(@RequestBody Todo tarefa) {
+    public Todo create(@RequestBody Todo tarefa) {
         return this.todoRepo.save(tarefa);
     }
 
@@ -41,6 +43,46 @@ public class TodoController {
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{todoId}/start_task")
+    public ResponseEntity<Todo> startTask(@PathVariable Integer todoId) {
+        Todo todoDataBase = this.todoRepo.findById(todoId).get();
+        if (todoDataBase != null) {
+            todoDataBase.setStatus(StatusEnum.IN_PROGRESS);
+            this.todoRepo.save(todoDataBase);
+            return ResponseEntity.ok(todoDataBase);
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{todoId}/end_task")
+    public ResponseEntity<Todo> endTask(@PathVariable Integer todoId) {
+        Todo todoDatabase = this.todoRepo.findById(todoId).get();
+        if (todoDatabase != null) {
+            todoDatabase.setStatus(StatusEnum.FINISHED);
+            this.todoRepo.save(todoDatabase);
+            return ResponseEntity.ok(todoDatabase);
+
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{todoID}/")
+    public ResponseEntity<Todo> uptade(@PathVariable Integer todoId, @RequestBody Todo todo) {
+        Todo todoDatabase = this.todoRepo.findById(todoId).get();
+        if (todoDatabase != null) {
+            todoDatabase.setTitle(todo.getTitle());
+            todoDatabase.setDescription(todo.getDescription());
+            this.todoRepo.save(todoDatabase);
+            return ResponseEntity.ok(todoDatabase);
+        } else {
+           return ResponseEntity.notFound().build();
         }
     }
 }
